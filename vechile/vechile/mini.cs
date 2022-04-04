28c4d69192192849735affa234ce4538c8e8ba58
rachit3014 <84663169+rachit3014@users.noreply.github.com>
2022-04-04 14:19:49 +0530

    Add files via upload

---
+++ vechile/vechile/mini.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vechile
+{
+    public class mini
+    {
+        protected int cargo_Net;
+        protected bool dual_Sliding;
+
+        public bool HasCargoNet() { return true; }
+        public bool HasDualSliding() { return dual_Sliding; }
+
+    }
+}
